f27a19e4ae685c019b6947c5e74a760fb90f3f7a
jongbin-choi <kooma1123@naver.com>
2022-04-18 17:19:49 +0900

    2022-04-18 반복문1차

--- src/com/javaex/ex04/Ex09.java
+++ src/com/javaex/ex04/Ex09.java
@@ -8,9 +8,9 @@ public class Ex09 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("숫자를 입력하세요.");
-		int num = sc.nextInt();
 		
 		while(true) {
+			int num = sc.nextInt();
 			if(num == 0) {
 				System.out.println("종료");
 				break;
@@ -18,12 +18,10 @@ public class Ex09 {
 			else if(num % 3 == 0) {
 				System.out.println("3의 배수입니다.");
 				System.out.println("숫자를 입력하세요.");
-				num = sc.nextInt();
 			}
 			else {
 				System.out.println("3의 배수가 아닙니다.");
 				System.out.println("숫자를 입력하세요.");
-				num = sc.nextInt();
 			}
 		}
 		sc.close();
